2dcd8539da2ba919841253051b9352f9e0852530
Remi <131499621+RopSEA@users.noreply.github.com>
2025-02-24 23:20:22 -0500

    Fix Enemy Health

--- Oblivions Crucible/Assets/Scripts/BasicEnemyMovement.cs
+++ Oblivions Crucible/Assets/Scripts/BasicEnemyMovement.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class BasicEnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     public float speed;
+    public int hp;
     public GameObject player;
+
+
+    public void damage(int dam)
+    {
+
+        if (hp - dam > 0)
+        {
+            hp = hp - dam;
+        }
+        else
+        {
+            hp = 0;
+        }
+
+        if (hp == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
