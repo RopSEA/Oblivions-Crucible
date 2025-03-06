10a2fd70330f13b0b8ffcc345c8be0cad692dc12
Remi <131499621+RopSEA@users.noreply.github.com>
2025-03-06 01:14:55 -0500

    CooldownsSpeedster

--- Oblivions Crucible/Assets/Scripts/SpeedsterClass.cs
+++ Oblivions Crucible/Assets/Scripts/SpeedsterClass.cs
@@ -1,14 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeedsterClass : Classes
 {
+
+    public float priCooldown;
+    public float secCooldown;
+    private float lastPri;
+    private float lastSec;
+    private bool pri = false;
+    private bool sec = false;
+    public Image Img;
+    public Image Img2;
+
+
+    IEnumerator priCor()
+    {
+        float cool = priCooldown;
+        Img.fillAmount = 1;
+        while (cool >  0)
+        {
+            Img.fillAmount -= 0.5f / priCooldown;
+            yield return new WaitForSeconds(0.5f);
+            cool -= 0.5f;
+        }
+        Debug.Log("READY PRI");
+        pri = false;
+    }
+
+    IEnumerator secCor()
+    {
+        float cool = secCooldown;
+        Img2.fillAmount = 1;
+        while (cool > 0)
+        {
+            Img2.fillAmount -= 0.5f / secCooldown;
+            yield return new WaitForSeconds(0.5f);
+            cool -= 0.5f;
+        }
+        Debug.Log("READY SEC");
+        sec = false;
+    }
+
     public override void priSkill()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            if (pri == true)
+            {
+                return;
+            }
+            pri = true;
             Debug.Log("PRIMARY");
+            StartCoroutine(priCor());
         }
     }
 
@@ -16,11 +63,17 @@ public class SpeedsterClass : Classes
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
+            if (sec == true)
+            {
+                return;
+            }
+            sec = true;
             Debug.Log("SECONDARY");
+            StartCoroutine(secCor());
         }
     }
 
-    // Start is called before the first frame update
+
     void Start()
     {
         
