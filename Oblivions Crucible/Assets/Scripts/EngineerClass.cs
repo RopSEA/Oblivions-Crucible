f0604e385c57ed922d4fd60ab145eb19fcdebb99
Remi <131499621+RopSEA@users.noreply.github.com>
2025-03-09 21:01:22 -0400

    Revert "Finish Engineer Class"
    This reverts commit 5fb1f4f87da53e99043386a4a21396eb89988c0c.

--- Oblivions Crucible/Assets/Scripts/EngineerClass.cs
+++ Oblivions Crucible/Assets/Scripts/EngineerClass.cs
@@ -1,103 +1,32 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class EngineerClass : Classes
 {
-    public float priCooldown;
-    public float secCooldown;
-    public Image Img;
-    public Image Img2;
-
-
-    private bool pri = false;
-    private bool sec = false;
-    public GameObject turretPrefab;
-    private GameObject currTurr;
-    public Transform turretLoc;
-    public GameObject bulletPrefab;
-
     public override void priSkill()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (pri == false)
-            {
-                Debug.Log("PRIMARY Engineer");
-                pri = true;
-                if (currTurr != null)
-                {
-                    Destroy(currTurr);
-                    currTurr = null;
-                }
-                currTurr = Instantiate(turretPrefab, turretLoc.position, turretLoc.rotation);
-                StartCoroutine(priCor());
-            }
-            
+            Debug.Log("PRIMARY Engineer");
         }
     }
 
     public override void secSkill()
     {
-        int rot = 0;
-        GameObject temp;
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (sec == false)
-            {
-                Debug.Log("SECONDARY Engineer");
-                sec = true;
-
-                for (int i = 0; i < 8; i++)
-                {
-                    temp = Instantiate(bulletPrefab, transform.position, transform.rotation);
-                    temp.GetComponent<ForwardBullet>().updateDir(rot);
-                    rot += 45;
-                }
-
-                // Cooldown
-                StartCoroutine(secCor());
-            }
-            
+            Debug.Log("SECONDARY Engineer");
         }
     }
-    IEnumerator priCor()
-        {
-            float cool = priCooldown;
-            Img.fillAmount = 1;
 
-            while (cool > 0)
-            {
-                Img.fillAmount -= 0.5f / priCooldown;
-                yield return new WaitForSeconds(0.5f);
-                cool -= 0.5f;
-            }
-            Debug.Log("READY PRI");
-            pri = false;
-        }
-
-    IEnumerator secCor()
-    {
-         float cool = secCooldown;
-         Img2.fillAmount = 1;
-
-         while (cool > 0)
-         {
-             Img2.fillAmount -= 0.5f / secCooldown;
-             yield return new WaitForSeconds(0.5f);
-             cool -= 0.5f;
-         }
-
-         Debug.Log("READY SEC");
-         sec = false;
-    }
-
+    // Start is called before the first frame update
     void Start()
     {
 
     }
+
+    // Update is called once per frame
     void Update()
     {
         priSkill();
